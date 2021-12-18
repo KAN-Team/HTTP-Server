69ad8e9319582b2a6f8eb740ae232e82213ae5b3
Nada elsayed anies <nada.said39@gmail.com>
2021-12-19 00:25:18 +0200

    starting modifing request class

--- HTTPServer/Request.cs
+++ HTTPServer/Request.cs
@@ -47,20 +47,28 @@ namespace HTTPServer
         {
             throw new NotImplementedException();
 
-            //TODO: parse the receivedRequest using the \r\n delimeter   
-
+            //TODO: parse the receivedRequest using the \r\n delimeter
+            string[] stringSeparators = new string[] { "\r\n" };
+            requestLines = requestString.Split(stringSeparators, StringSplitOptions.None);
             // check that there is atleast 3 lines: Request line, Host Header, Blank line (usually 4 lines with the last empty line for empty content)
-
+            bool NumberOfLines = false;
+            if (requestLines.Length == 4) NumberOfLines = true;
             // Parse Request line
-
+            bool Validrequestline = ParseRequestLine();
             // Validate blank line exists
 
             // Load header lines into HeaderLines dictionary
+
+        
         }
 
         private bool ParseRequestLine()
         {
             throw new NotImplementedException();
+            string[] ReqLineDetails = requestLines[0].Split(' ');
+
+            bool MethodValid = Enum.IsDefined(typeof(RequestMethod), ReqLineDetails[0]);
+            
         }
 
         private bool ValidateIsURI(string uri)
